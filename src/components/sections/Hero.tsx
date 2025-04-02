e3751344c7313eaea5a51b32a41e3131e58ec321
SANDESH GURURAJ KULLOLLI <sandeshgururajkullolli_cse2022@ksit.edu.in>
2025-04-02 21:01:31 +0530

    Update Hero.tsx

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -41,13 +41,15 @@ const Hero: React.FC<HeroProps> = ({
           animate={{ opacity: 1, x: 0 }} 
           transition={{ duration: 0.8, ease: "easeOut" }}
         >
-          <motion.h1
-          className="text-5xl md:text-6xl mb-6 text-gradient font-extrabold text-left lg:text-7xl
-             p-4 rounded-xl bg-black/10 shadow-[0_0_35px_rgba(255,255,255,0.3)]
-             transition-shadow duration-300"
-             >
-           {profile.name}
-            </motion.h1>
+         <motion.h1
+  style={{
+    textShadow: "0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff"
+  }}
+  className="text-5xl md:text-6xl mb-6 text-white font-extrabold text-left lg:text-7xl"
+>
+  {profile.name}
+</motion.h1>
+
 
           
           <motion.h2 
